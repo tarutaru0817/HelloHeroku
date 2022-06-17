ee659f1a6c3db647ec272dedc8b5b35b0a51afe0
tarutaru0817 <tarutaru.c@outlook.com>
2022-06-17 08:14:32 +0000

    C

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::group(['middleware' => 'auth'], function(){
 
     Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
     Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
     Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
 
+ Route::group(['middleware' => 'can:view,folder'], function() {
 
     Route::get('/folders/{folder}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
     Route::get('/folders/{folder}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
@@ -25,7 +26,8 @@ use Illuminate\Support\Facades\Route;
     Route::get('/folders/{folder}/tasks/{task}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
     Route::post('/folders/{folder}/tasks/{task}/edit', 'App\Http\Controllers\TaskController@edit');
 
-
+  });
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
